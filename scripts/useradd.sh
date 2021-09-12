07f940a666c5dd182414856e6cca3a5d10f1238c
legymhueck <le.gymhueck@gmail.com>
2021-09-12 19:13:01 +0200

    encrypted scripts

--- scripts/useradd.sh
+++ scripts/useradd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-useradd -m -u 60102 -g users -G wheel,audio,video,disk,storage,optical,scanner,rfkill,input -s /bin/bash michael
+useradd -m -u 60102 -g users -G wheel,audio,video,disk,storage,optical,scanner,rfkill,input,libvirt,kvm -s /bin/bash michael
