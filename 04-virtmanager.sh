a16c9d3971dc8f76d8863afac4185030facc8bda
legymhueck <le.gymhueck@gmail.com>
2021-10-20 17:06:56 +0200

    virt manager additions from youtube video

--- 04-virtmanager.sh
+++ 04-virtmanager.sh
@@ -8,6 +8,9 @@ sudo vim /etc/libvirt/libvirtd.conf
 # line 81: unix_sock_group = "libvirt"
 # uncomment line 104: unix_sock_rw_perms = "0770"
 
+sudo vim /etc/libvirt/qemu.conf
+# user = "michael"
+# group = "users"
 
 #sudo systemctl restart --now libvirtd
 #sudo virsh net-autostart default
@@ -20,3 +23,18 @@ sudo vim /etc/libvirt/libvirtd.conf
 #options kvm_intel nested=1
 #options kvm_intel emulate_invalid_guest_state=0
 #options kvm ignore_msrs=1 report_ignored_msrs=0
+
+# Take snapshots of UEFI drives
+# sudo su
+# export EDITOR=vim
+# virsh edit {name of vm}
+# replace: <loader readonly='yes' type='pflash'>/usr/share/edk2/ovmf/OVMF_CODE.fd</loader>
+# with:    <loader readonly='yes' type='rom'>/usr/share/edk2-ovmf/x64/OVMF_CODE.fd</loader>
+
+
+# Edit the xml and add
+# </features>
+#  <cpu mode='host-passthrough' check='none'>
+#    <topology sockets='1' cores='6' threads='2'/>
+#    <feature policy='disable' name='smep'/>
+#  </cpu>
